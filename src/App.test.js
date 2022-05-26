d7d76e5e831632b40911d19ccc58be1f3dd53c45
KS-20 <62520720+KS-20@users.noreply.github.com>
2022-05-26 16:51:05 +0300

    Add Jest snaphot test, test that the generated HTML of the page when the app starts up does not change

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,36 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+globalThis.IS_REACT_ACT_ENVIRONMENT = true; //see:  https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html#configuring-your-testing-environment
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import React from "react";
+import { act } from "react-dom/test-utils";
+import pretty from "pretty";
+import ReactDOM from 'react-dom/client';
+
+
+import App from './App';
+import {appEngine} from './source.js'
+
+let container = null;
+let root = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  // unmountComponentAtNode(container);
+  // root.unmount();
+  container.remove();
+  container = null;
+});
+
+test("Snapshot of initial page", () => {
+  act(() => {
+    root = ReactDOM.createRoot(container);
+    root.render(<App appEngine={appEngine}/>);  
+
+  });
+
+  expect(pretty(container.innerHTML)).toMatchSnapshot(); 
 });
